35eccef7a6509d16914636a53212602e2a862b1e
Nikolai Wotton <wottonn@gmail.com>
2026-01-22 10:57:23 -0500

    partial reversion

--- Package.swift
+++ Package.swift
@@ -38,25 +38,25 @@ var targets: [PackageDescription.Target] = [
         name: "CandleNIOHTTPCompression",
         dependencies: [
             "CandleCNIOExtrasZlib",
-            .product(name: "CandleNIO", package: "swift-nio"),
-            .product(name: "CandleNIOCore", package: "swift-nio"),
-            .product(name: "CandleNIOHTTP1", package: "swift-nio"),
+            .product(name: "CandleNIO",package: "candle-swift-nio"),
+            .product(name: "CandleNIOCore",package: "candle-swift-nio"),
+            .product(name: "CandleNIOHTTP1",package: "candle-swift-nio"),
         ],
         swiftSettings: strictConcurrencySettings
     ),
     .target(
         name: "CandleNIOSOCKS",
         dependencies: [
-            .product(name: "CandleNIO", package: "swift-nio"),
-            .product(name: "CandleNIOCore", package: "swift-nio"),
+            .product(name: "CandleNIO",package: "candle-swift-nio"),
+            .product(name: "CandleNIOCore",package: "candle-swift-nio"),
         ],
         swiftSettings: strictConcurrencySettings
     ),
     .executableTarget(
         name: "NIOSOCKSClient",
         dependencies: [
-            .product(name: "CandleNIOCore", package: "swift-nio"),
-            .product(name: "CandleNIOPosix", package: "swift-nio"),
+            .product(name: "CandleNIOCore",package: "candle-swift-nio"),
+            .product(name: "CandleNIOPosix",package: "candle-swift-nio"),
             "CandleNIOSOCKS",
         ],
         swiftSettings: strictConcurrencySettings
@@ -73,10 +73,10 @@ var targets: [PackageDescription.Target] = [
         dependencies: [
             "CandleCNIOExtrasZlib",
             "CandleNIOHTTPCompression",
-            .product(name: "CandleNIOCore", package: "swift-nio"),
-            .product(name: "CandleNIOEmbedded", package: "swift-nio"),
-            .product(name: "CandleNIOHTTP1", package: "swift-nio"),
-            .product(name: "CandleNIOConcurrencyHelpers", package: "swift-nio"),
+            .product(name: "CandleNIOCore",package: "candle-swift-nio"),
+            .product(name: "CandleNIOEmbedded",package: "candle-swift-nio"),
+            .product(name: "CandleNIOHTTP1",package: "candle-swift-nio"),
+            .product(name: "CandleNIOConcurrencyHelpers",package: "candle-swift-nio"),
         ],
         swiftSettings: strictConcurrencySettings
     ),
@@ -84,15 +84,15 @@ var targets: [PackageDescription.Target] = [
         name: "NIOSOCKSTests",
         dependencies: [
             "CandleNIOSOCKS",
-            .product(name: "CandleNIOCore", package: "swift-nio"),
-            .product(name: "CandleNIOEmbedded", package: "swift-nio"),
+            .product(name: "CandleNIOCore",package: "candle-swift-nio"),
+            .product(name: "CandleNIOEmbedded",package: "candle-swift-nio"),
         ],
         swiftSettings: strictConcurrencySettings
     ),
     .target(
         name: "NIONFS3",
         dependencies: [
-            .product(name: "CandleNIOCore", package: "swift-nio")
+            .product(name: "CandleNIOCore",package: "candle-swift-nio")
         ],
         swiftSettings: strictConcurrencySettings
     ),
